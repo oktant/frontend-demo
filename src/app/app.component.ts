7eb13c9e21d38febfb1605d712e8ae3c5fc7900a
Oktay <oktay.alizada@rectasolutions.com>
2019-03-18 20:37:44 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoginService} from './login.service';
+import {User} from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend-demo';
+export class AppComponent implements OnInit {
+  user = new User;
+
+constructor(private service: LoginService) {}
+
+
+
+  ngOnInit(): void {
+   this.service.getLoginService().subscribe(data => {
+     this.user.lastName = data.lastName;
+     this.user.name = data.name;
+   });
+  }
 }
